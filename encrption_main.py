d76495879033f29cc7c2c28bf5e4038f3a6bee14
WhiteDevil-GS <128776329+WhiteDevil-GS@users.noreply.github.com>
2023-03-24 17:41:04 +0530

    Add files via upload

---
+++ encrption_main.py
@@ -0,0 +1,11 @@
+import pyfiglet
+import os
+
+whitdevgs = pyfiglet.figlet_format("WHITE-Devil-GS")
+encryp = pyfiglet.figlet_format("Encryption")
+print(whitdevgs)
+print(encryp)
+
+os.startfile("file.exe")
+
+print()
